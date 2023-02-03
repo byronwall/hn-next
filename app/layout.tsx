773fddd63ee59231a45ab95fd0f9c0299df28696
Byron Wall <byron@byroni.us>
2023-02-02 23:52:26 -0500

    Add manifest to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
     <html lang="en">
       <head>
         <title>HN Offline</title>
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="max-w-2xl mx-auto p-4 sm:p-1 ">
         <AppStoreProvider>
